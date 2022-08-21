e50a7946a14e36c84b0ac8ad9c8988f03098739d
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-08-21 15:55:57 -0300

    feat: :sparkles: Created first interfaces and connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import UserRoutes from './routes/user.routes';
+import ProductRoutes from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(ProductRoutes);
+app.use(UserRoutes);
 
 export default app;
---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,9 @@
+interface Product {
+  id: number;
+  name: string;
+  amount: string;
+  orderId: number;
+  
+}
+
+export default Product;
